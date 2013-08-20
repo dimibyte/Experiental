82d99389a891b34dfc28f33b4a7a6d99958df960
dimibyte <dimibyte@gmail.com>
2013-08-21 00:49:23 +0300

    attached notices about license

--- src/system/dator.h
+++ src/system/dator.h
@@ -1,3 +1,20 @@
+/*
+ *  This file is part of Experiental.
+ *
+ *  Experiental is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Experiental is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Experiental.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 ///A dator is an object which wraps a data member
 //Dators will be useful later on for scripting, but right now, we're going to use them for our settings mechanism.
 
--- src/system/functor.cpp
+++ src/system/functor.cpp
@@ -1,3 +1,20 @@
+/*
+ *  This file is part of Experiental.
+ *
+ *  Experiental is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Experiental is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Experiental.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 ///The Functor allows us to wrap up a function inside an object.
 ///It could be useful for, say, callback handlers when a button is pressed, in a UI system.
 
